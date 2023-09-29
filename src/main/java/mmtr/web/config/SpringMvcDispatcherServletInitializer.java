301c428694ee44611db06031c5303e0c6fefa45a
dyakonov.dd.kst <dyakonov.dd@mmtr.ru>
2023-09-29 11:14:03 +0300

    init commit

---
+++ src/main/java/mmtr/web/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package mmtr.web.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
